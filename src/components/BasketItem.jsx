13a46f4ab697f7e9e00ed6f83d63d829879dc1e5
Max Faterin <faterin01032004@gmail.com>
2023-06-03 18:49:25 +0300

    первый коммит

---
+++ src/components/BasketItem.jsx
@@ -0,0 +1,16 @@
+function BasketItem(props) {
+    const { id, name, price, full_background, quantity, removeFromBasket = Function.prototype, incQuantity = Function.prototype, decQuantity = Function.prototype, } = props;
+    return (
+        <ul className="collection">
+            <li className="collection-item">
+                <span class="BasketName"> <img src={full_background} alt="back"/> {name} <i class="material-icons basket-quantity" onClick={() => decQuantity(id)}>remove</i> x {quantity}
+                <i class="material-icons basket-quantity" onClick={() => incQuantity(id)}>add</i> = {price * quantity} руб.
+                <span class="secondary-content" onClick={() => removeFromBasket(id)}>
+                    <i class="material-icons basket-delete">clear</i>
+                </span></span>
+            </li>
+        </ul>
+    );
+}
+
+export { BasketItem };
